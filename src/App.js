955349ea3d913c589330553d2157481ec5095485
Ayush Jain <ayush.ayushjain12@gmail.com>
2021-01-23 20:50:03 +0530

    added dummy dashboard

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import DisplayCart from './component/cart/cart.compoennt';
 import HeaderFooter from './component/Header.component';
 import {Route, BrowserRouter, Switch} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from './store/store'
+import store from './store/store';
+
+const Dashboard = () => <div>Dashborad</div>
 function App() {
   return (  
       <Provider store={store}>
         <BrowserRouter>
             <HeaderFooter/>
             <Switch>
-                <Route path='/' exact component ={OrderCatalogue}/>
-                <Route path='/orderCatalogue'  exact component ={OrderCatalogue}/>
+                <Route path='/' exact component ={Dashboard}/>
+                <Route path='/Catalogue'  exact component ={OrderCatalogue}/>
                 <Route path='/cart' exact component ={DisplayCart}/>
             </Switch>
             <HeaderFooter isFooter/>
